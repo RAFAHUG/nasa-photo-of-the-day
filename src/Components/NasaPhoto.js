cbe06837a2594d57a9547a9451f78751f07fee69
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2022-12-09 15:23:42 -0600

    style-component use

--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,13 +1,66 @@
 import React from 'react' ; 
-  
-const NasaPhoto = (props) => {
+import styled from 'styled-components'; 
+
+const NasaPhotoWrapper = styled.div `
+    background:#FA8072; 
+    padding: 50px;
+    margin: 90px;
+    color: #B03A2E;
+`;
+
+const NasaPhotoh3 = styled.h3 `
+    display: flex ; 
+    justify-content: center ;
+    align-content: center ; 
+    margin-right: 30%;
+    margin-left: 30%;
+    font-size: 160% ;  
+    font-style: italic ; 
+    font-family: sans-serif ; 
+    border-style: solid ;
+    border-color: #CD5C5C;
+    border-radius: 20px ; 
+    width: 30rem;
+    
+`;
+
+const NasaPhotodate = styled.p `
+    font-family: sans-serif ;
+    font-size: 100% ; 
+    border-bottom: solid; 
+    border-color: #CD5C5C;
+    padding: 30px;
+    margin-left: 10% ; 
+    margin-right: 10% ; 
+`;
+
+const NasaPhotoimg = styled.img `
+    width: 400px;
+    height: 400px;
+    position: relative;
+    overflow: hidden;
+    border-radius: 50%;
+`;
+
+const NasaExplanation = styled.p `
+    text-align: justify;
+    margin-left: 17%;
+    margin-right: 17%;
+    line-height: 40px;
+    font-family: sans-serif;
+    font-size: 20px;
+    font-style: italic;
+`;
+
+function NasaPhoto (props)  {
+
     return (
-        <div className='nasa-photo-wrapper'>
-            <h3>{props.photo.title}</h3>
-            <p>{props.photo.date}</p>
-            <img src={props.photo.hdurl}/>
-            <p>{props.photo.explanation}</p>
-        </div>
+        <NasaPhotoWrapper>
+            <NasaPhotoh3>{props.photo.title}</NasaPhotoh3>
+            <NasaPhotodate>{props.photo.date}</NasaPhotodate>
+            <NasaPhotoimg src={props.photo.hdurl}></NasaPhotoimg>
+            <NasaExplanation>{props.photo.explanation}</NasaExplanation>
+        </NasaPhotoWrapper>
     );
 }
 
